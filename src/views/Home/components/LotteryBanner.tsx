ab3c12672a52be8b41d8fc24b668575fdec7fa4d
sauceisgood <53635512+sauceisgood@users.noreply.github.com>
2021-07-09 17:27:20 +0100

    Update LotteryBanner.tsx

--- src/views/Home/components/LotteryBanner.tsx
+++ src/views/Home/components/LotteryBanner.tsx
@@ -120,7 +120,7 @@ const LotteryBanner: React.FC<{ currentLotteryPrize: string }> = ({ currentLotte
               {inPrizes}
             </Text>
           </PrizeFlex>
-          <a href="https://t.me/miniboxer">Telegram</a>
+          <NavLink exact activeClassName="active" to="/lottery" id="lottery-pot-banner">
             <Button>
               <Text color="white" bold fontSize="16px" mr="4px">
                 {t('Telegram')}
